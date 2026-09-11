df8d583bce0d5b83ac4c0e6897be6b422280befa
GuilhermeAdamDeSouza <guilhermesouzaadam@gmail.com>
2026-09-11 12:20:55 -0400

    aula-05: Maquina estados

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,112 +7,24 @@ enum PlayerState {
 	duck
 }
 
+@export var max_jump_count: int = 2
+@export var SPEED: float = 150.0
+@export var JUMP_VELOCITY: float = -300.0
+
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-var direction = 0
-
-const SPEED = 120.0
-const JUMP_VELOCITY = -300.0
-
-var jump_count = 0
-var max_jump_count = 2
-
+var direction: float = 0.0
+var jump_count: int = 0
 var status: PlayerState
 
 func _ready() -> void:
 	go_to_idle_state()
 
-func go_to_idle_state():
-	status = PlayerState.idle
-	anim.play("idle")
-
-func go_to_walk_state():
-	status = PlayerState.walk
-	anim.play("walk")
-
-func go_to_duck_state():
-	status = PlayerState.duck
-	anim.play("duck")
-	collision_shape.shape.radius = 6
-	collision_shape.shape.height = 12
-	collision_shape.position.y = 4
-
-func exit_duck_state():
-	collision_shape.shape.radius = 6
-	collision_shape.shape.height = 16
-	collision_shape.position.y = 3
-
-func go_to_jump_state():
-	jump_count += 1
-	
-	status = PlayerState.jump
-	anim.play("jump")
-	velocity.y = JUMP_VELOCITY
-
-func duck_state():
-	update_direction()
-	if Input.is_action_just_released("duck"):
-		go_to_idle_state()
-		exit_duck_state()
-		return
-
-func idle_state():
-	move()
-	
-	if velocity.x != 0:
-		go_to_walk_state()
-		return
-	if Input.is_action_just_pressed("jump"):
-		go_to_jump_state()
-		return
-	if Input.is_action_just_pressed("duck"):
-		go_to_duck_state()
-		return
-
-func walk_state():
-	move()
-	
-	if velocity.x == 0:
-		go_to_idle_state()
-		return
-	if Input.is_action_just_pressed("jump"):
-		go_to_jump_state()
-		return
-
-func jump_state():
-	move()
-	
-	if Input.is_action_just_pressed("jump") and jump_count < max_jump_count:
-		go_to_jump_state()
-		return
-	
-	if is_on_floor():
-		jump_count = 0
-		if velocity.x == 0:
-			go_to_idle_state()
-		else:
-			go_to_walk_state()
-		return
-
-func move():
-	update_direction()
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-func update_direction():
-	direction = Input.get_axis("left", "right")
-	if direction > 0:
-		anim.flip_h = false
-	elif direction < 0:
-		anim.flip_h = true
-
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
+
 	match status:
 		PlayerState.idle:
 			idle_state()
@@ -122,38 +34,97 @@ func _physics_process(delta: float) -> void:
 			jump_state()
 		PlayerState.duck:
 			duck_state()
-	
+
 	move_and_slide()
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("jump") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
+#FUNÇÕES DE ENTRADA E SAÍDA DE ESTADOS
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("left", "right")
-	#if direction:
-	#	velocity.x = direction * SPEED
-	#else:
-	#	velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	#if is_on_floor():
-	#	if direction > 0:
-	#		anim.flip_h = false
-	#		anim.play("run")
-	#	elif direction < 0:
-	#		anim.flip_h = true
-	#		anim.play("run")
-	#	else:
-	#		anim.play("idle")
-	#else:
-	#	anim.play("jump")
-	#	if direction > 0:
-	#		anim.flip_h = false
-	#	elif direction < 0:
-	#		anim.flip_h = true
-	#move_and_slide()
+func go_to_idle_state() -> void:
+	status = PlayerState.idle
+	anim.play("idle")
 
+func go_to_walk_state() -> void:
+	status = PlayerState.walk
+	anim.play("walk")
 
-func _on_child_entered_tree(_node: Node) -> void:
-	pass # Replace with function body.
+func go_to_duck_state() -> void:
+	status = PlayerState.duck
+	anim.play("duck")
+	collision_shape.shape.radius = 5.0
+	collision_shape.shape.height = 10.0
+	collision_shape.position.y = 3.0
+
+func exit_from_duck_state() -> void:
+	collision_shape.shape.radius = 6.0
+	collision_shape.shape.height = 16.0
+	collision_shape.position.y = 0.0
+
+func go_to_jump_state() -> void:
+	jump_count += 1
+	status = PlayerState.jump
+	anim.play("jump")
+	velocity.y = JUMP_VELOCITY
+
+#LÓGICA DOS ESTADOS (PERMANÊNCIA E TRANSIÇÃO)
+
+func idle_state() -> void:
+	move()
+
+	if velocity.x != 0:
+		go_to_walk_state()
+		return
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+	if Input.is_action_pressed("duck"):
+		go_to_duck_state()
+		return
+
+func walk_state() -> void:
+	move()
+
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+
+func duck_state() -> void:
+	update_direction()
+
+	if Input.is_action_just_released("duck"):
+		exit_from_duck_state()
+		go_to_idle_state()
+		return
+
+func jump_state() -> void:
+	move()
+
+	if Input.is_action_just_pressed("jump") and jump_count < max_jump_count:
+		go_to_jump_state()
+		return
+
+	if is_on_floor():
+		jump_count = 0
+		if velocity.x == 0:
+			go_to_idle_state()
+		else:
+			go_to_walk_state()
+		return
+
+#FUNÇÕES AUXILIARES
+
+func update_direction() -> void:
+	direction = Input.get_axis("left", "right")
+	if direction < 0:
+		anim.flip_h = true
+	elif direction > 0:
+		anim.flip_h = false
+
+func move() -> void:
+	update_direction()
+	if direction != 0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
